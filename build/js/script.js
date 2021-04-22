a37f872877d1bd86a0486c446480b6ca0f1d2bb3
Galina Popova <g89117301783@gmail.com>
2021-04-22 20:52:34 +0300

    Добавит: вернула всё назад  в блоке company

--- build/js/script.js
+++ build/js/script.js
@@ -26,6 +26,35 @@
 
 })();
 
+`use strict`;
+
+// Хранение данных в localStorage
+const phoneInput = document.querySelector(`#phone`);
+const phoneInputModal = document.querySelector(`#phone-modal`);
+
+const contactsForm = document.querySelector(`#contacts .form`);
+const contactsFormModal = document.querySelector(`.modal .form`);
+
+if (contactsForm) {
+  contactsForm.addEventListener(`submit`, function (event) {
+    event.preventDefault();
+
+    localStorage.setItem(`name-field`, nameInput.value);
+    localStorage.setItem(`phone-field`, phoneInput.value);
+    localStorage.setItem(`message-field`, messageInput.value);
+  });
+}
+
+if (contactsFormModal) {
+  contactsFormModal.addEventListener(`submit`, function (event) {
+    event.preventDefault();
+
+    localStorage.setItem(`name-modal`, nameInputModal.value);
+    localStorage.setItem(`phone-modal`, phoneInputModal.value);
+    localStorage.setItem(`message-modal`, messageInputModal.value);
+  });
+}
+
 'use strict';
 
   // Управление отображением модального окна
