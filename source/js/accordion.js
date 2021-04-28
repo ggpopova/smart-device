9c3acd96ea92ae3e47e58d8cb4db05b60957d897
Galina Popova <g89117301783@gmail.com>
2021-04-28 19:32:20 +0300

    Поправит переключение стилей у раскрывающего списка в подвале

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -2,26 +2,18 @@
 
 // аккордион на mobile в футере
 (function () {
-  const accordion = document.querySelectorAll(`.accordion__button`);
-  const panels = document.querySelectorAll(`.accordion__panel`);
+  const accordionButtons = document.querySelectorAll(`.accordion__button`);
 
   document.querySelector(`.accordion__panel--no-js`).classList.remove(`accordion__panel--no-js`);
 
-  if (accordion) {
-    for (let i = 0; i < accordion.length; i++) {
-      accordion[i].addEventListener(`click`, function (evt) {
-
-        for (let j = 0; j < panels.length; j++) {
-          panels[j].classList.remove(`accordion__panel--active`);
-        }
-
-        const button = evt.target;
-        button.classList.toggle(`accordion__button--active`);
-
-        const panel = button.nextElementSibling;
-        panel.classList.toggle(`accordion__panel--active`);
+  if (accordionButtons) {
+    accordionButtons.forEach((accordionButton) => {
+      accordionButton.addEventListener(`click`, () => {
+        accordionButton.classList.toggle(`accordion__button--active`);
+        const accordionPanel = accordionButton.nextElementSibling;
+        accordionPanel.classList.toggle(`accordion__panel--active`);
       });
-    }
+    });
   }
 
 })();
